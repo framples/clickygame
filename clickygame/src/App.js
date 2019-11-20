d0af116be542966e6a98949d6645af243eafe70e
Illio, the Great <pwfcoding@gmail.com>
2019-11-19 20:35:41 -0600

    initial commit

---
+++ clickygame/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import ChampCard from "./components/ChampCard";
+import Instructions from "./components/Instructions";
+import Score from "./components/Score";
+import Wrapper from "./components/Wrapper";
+import leagechamps from "./leaguechamps.json";
+
+class App extends Component {
+  state = {
+    leaguechamps
+  };
+
+
+  
+}
+
+
+
+export default App;
---
+++ clickygame/src/components/ChampCard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function ChampCard(props) {
+    return {
+
+    }
+}
+
+export default ChampCard;
