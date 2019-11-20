f03a1254617ea4bfa0614c1e120f61333cd5ea94
unknown <pwfcoding@gmail.com>
2019-11-20 15:19:01 -0600

    images rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import ChampCard from "./components/ChampCard";
 import Instructions from "./components/Instructions";
-import Score from "./components/Score";
+//import Score from "./components/Score";
 import Wrapper from "./components/Wrapper";
-import leagechamps from "./leaguechamps.json";
+import leaguechamps from "./leaguechamps.json";
 
 class App extends Component {
   state = {
     leaguechamps
   };
 
+  render () {
+    return (
+      <Wrapper>
+        <Instructions>Clicky Game</Instructions>
+        {this.state.leaguechamps.map(champ => (
+          <ChampCard
+          id={champ.id}
+          name={champ.name}
+          image={champ.image}
+          />
+        ))}
+      </Wrapper>
+    )
+  }
 
   
 }
--- src/components/ChampCard/index.js
+++ src/components/ChampCard/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./style.css";
 
 function ChampCard(props) {
-    return {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={props.image} />
+            </div>
+        </div>
+    )
 
-    }
+    
 }
 
 export default ChampCard;
