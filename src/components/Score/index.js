597304b4cc9a6220fbc0cd0a3002b3653e0bfc2b
unknown <pwfcoding@gmail.com>
2019-11-21 20:48:14 -0600

    working code

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -5,16 +5,16 @@ import "./style.css";
 function Score(props) {
 
     return (
-        <div>
+        <div className="scoreboard">
             <div className="row top-row">
                 <div className="col s8 m8 l8">
                     <h6 className="clickMessage">{props.clickMessage}</h6>
                 </div>
                 <div className="col s1 m1 l1">
-                    <h6 className="correctClicks">{props.correctClicks}</h6>
+                    <h6 className="correctClicks">correct clicks: {props.correctClicks}</h6>
                 </div>
                 <div className="col s1 m1 l1">
-                    <h6 className="topScore">{props.topScore}</h6>
+                    <h6 className="topScore">top score: {props.topScore}</h6>
                 </div>
             </div>
         </div>
@@ -23,24 +23,3 @@ function Score(props) {
 }
 export default Score;
 
-
-
-
-
-
-
-/*function Score() {
-    return (
-        <nav className="navbar sticky-top navbar-dark bg-secondary" id="scoreboard">
-            <div className="col-12">
-                <div className="row">
-                    <div className="col-md-4">
-                        <h2 className="navi-3">Score: {Score.score} | Top Score: {Score.topScore}</h2>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
-};
-
-export default Score; */
